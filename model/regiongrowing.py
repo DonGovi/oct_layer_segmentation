5ba92188096adbe3be9902e0ff35e2028bb24c39
DonGovi <ykshisb@126.com>
2018-11-20 19:51:22 +0800

    Add files via upload

--- model/regiongrowing.py
+++ model/regiongrowing.py
@@ -19,12 +19,11 @@ class LayerSeg(object):
         self.coord = coord
         self.grad_img = self.imgGrad(self.img, kernel_size=9)
     
-    def imgGrad(self, img, kernel_size=9):
+    def imgGrad(self, kernel_size=9):
         '''
         Compute image gradient along Y axis
 
         Parameters:
-          img: (ndarray) image with (height, width, 3)
           kernel_size: (int) odd int for cv2.medianBlur's kernel
 
         Returns:
@@ -37,16 +36,15 @@ class LayerSeg(object):
         return gradY
     
     
-    def funcPhi(self, gradY, tau=1):
+    def funcPhi(self, tau=1):
         '''
         Energy function based on image gradient, larger gradient lead to smaller energy
 
         Parameters:
-          grad: (ndarray) gradient image with (height, width)
           tau: (float or int) weight parameter for energy
 
         Returns:
-         phi: (ndarray) energy array
+          phi: (ndarray) energy array
         '''
         phi = 1 / (1 + tau * np.power(self.grad_img, 2))
     
@@ -60,17 +58,16 @@ class LayerSeg(object):
     
         return coords
     
-    @staticmethod
-    def bdGrow(grad_img, start, area=5, tau=1):
+    def bdGrow(self, start, area=5, tau=1):
         height, width = grad_img.shape
         x, y = start
-        y_start = getField(y, area)
+        y_start = self.getField(y, area)
         x_start = np.array([x]*area)
-        start_energy = funcPhi(grad_img[y_start, x_start], tau)
+        start_energy = self.funcPhi(self.grad_img[y_start, x_start], tau)
         loc = np.argmin(start_energy)
         y += loc - int(area/2)
-        start = (x, y)
-        boundary = np.zeros(grad_img.shape[1], dtype=np.float32)
+        
+        boundary = np.zeros(self.grad_img.shape[1], dtype=np.float32)
         boundary[x] = y
         
         # Search from the start point to the front
@@ -78,10 +75,10 @@ class LayerSeg(object):
         forward_y = y
         while(forward_x > 0):
             forward_x -= 1
-            y_coords = getField(forward_y, area)
+            y_coords = self.getField(forward_y, area)
             x_coords = np.array([forward_x]*area)
-            grad = grad_img[y_coords, x_coords]
-            energy = funcPhi(grad, tau)
+            grad = self.grad_img[y_coords, x_coords]
+            energy = self.funcPhi(grad, tau)
             loc = np.argmin(energy)
             forward_y += loc - int(area/2)
             boundary[forward_x] = forward_y
@@ -91,10 +88,10 @@ class LayerSeg(object):
         backward_y = y
         while(backward_x+1 < width):
             backward_x += 1     # shift one pixel to right
-            y_coords = getField(backward_y, area)
+            y_coords = self.getField(backward_y, area)
             x_coords = np.array([backward_x]*area)
-            grad = grad_img[y_coords, x_coords]
-            energy = funcPhi(grad, tau)
+            grad = self.grad_img[y_coords, x_coords]
+            energy = self.funcPhi(grad, tau)
             loc = np.argmin(energy)
             backward_y += loc - int(area/2)
             boundary[backward_x] = backward_y
